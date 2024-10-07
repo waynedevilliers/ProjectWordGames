516101aa36d18df8ea188f7d894642e71dba77ca
Wayne de Villiers <wrdevilliers@gmail.com>
2024-10-07 09:49:40 +0200

    update

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,21 +1,13 @@
 const args = process.argv.slice(2);
-// Check if the user provided one word
-if (args.length !== 1) {
-  console.error(
-    "Please provide exactly one choice (Either rock, paper or scissors)"
-  );
-  return;
-}
-
 const userChoice = args[0].toLowerCase();
 //console.log(`You chose: ${userChoice}`);
 
 const computerChoice = ["rock", "paper", "scissors"];
 
 // Check if the user provided the correct word
-if (
+if (args.length !== 1 ||
   userChoice !== "rock" &&
-  userChoice !== "paper" &&
+  userChoice !== "paper"&&
   userChoice !== "scissors"
 ) {
   console.error("One word must be written,  either rock, paper or scissors");
@@ -28,22 +20,23 @@ const randomIndex = Math.floor(Math.random() * computerChoice.length);
 // Get the random choice from the array
 const randomChoice = computerChoice[randomIndex];
 
-
+// Check if user and the computer have the same choice
 if (userChoice === randomChoice) {
   console.log(
-    `You choose ${userChoice} and the computer choose ${randomChoice}. Nobody wins`
+    `You choose ${userChoice} and the computer choose ${randomChoice}. Nobody wins, try again!`
   );
+  // Check the winning probabilites
 } else if (
    (userChoice === "rock" && randomChoice === "scissors") || 
    (userChoice === "paper" && randomChoice === "rock") ||
    (userChoice === "scissors" && randomChoice === "paper"))
    {
   console.log(
-    `You choose ${userChoice} and the computer choose ${randomChoice}. You win`
+    `You choose ${userChoice} and the computer choose ${randomChoice}. You win, play again`
   );
 } else {
   console.log(
-    `You choose ${userChoice} and the computer choose ${randomChoice}. You lose!`
+    `You choose ${userChoice} and the computer choose ${randomChoice}. You lose, play again!`
   );
 }
 return;
